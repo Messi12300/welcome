a18880ad01286ea50a9b46c59caa222a9140853e
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-10-22 19:27:46 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -353,3 +353,60 @@ async def bot_info(bot, message):
         ]
     ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
+@Client.on_message(filters.new_chat_members)
+async def auto_welcome(bot: Pr0fess0r_99, msg: Message):
+    # from PR0FESS0R-99 import ID-Bot
+    first = msg.from_user.first_name
+    last = msg.from_user.last_name
+    mention = msg.from_user.mention
+    username = msg.from_user.username
+    id = msg.from_user.id
+    group_name = msg.chat.title
+    group_username = msg.chat.username
+    name_button = "🔰 JOIN NOW 🔰"
+    link_button = "https://t.me/MalluCinemas_Official"
+    button_name = "Welcome"
+    button_link = "https://t.me/MalluCinemas_Official"
+    welcome_text = f"Hey {mention}\nWelcome To {group_name}"
+    WELCOME_TEXT = f"Hi 😆"
+    print("Welcome Message Activate")
+    BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
+    if not BUTTON:
+       await msg.reply_text(text=WELCOME_TEXT.format(
+           first = msg.from_user.first_name,
+           last = msg.from_user.last_name,
+           username = None if not msg.from_user.username else '@' + msg.from_user.username,
+           mention = msg.from_user.mention,
+           id = msg.from_user.id,
+           group_name = msg.chat.title,
+           group_username = None if not msg.chat.username else '@' + msg.chat.username
+          )
+       )
+    else:
+       await msg.reply_text(text=WELCOME_TEXT.format(
+           first = msg.from_user.first_name,
+           last = msg.from_user.last_name,
+           username = None if not msg.from_user.username else '@' + msg.from_user.username,
+           mention = msg.from_user.mention,
+           id = msg.from_user.id,
+           group_name = msg.chat.title,
+           group_username = None if not msg.chat.username else '@' + msg.chat.username
+          ),
+       reply_markup=InlineKeyboardMarkup(
+               [
+                   [
+                       InlineKeyboardButton
+                           (
+                               button_name, url=f"https://t.me/MalluCinemas_Official"
+                           )
+                   ]  
+               ]
+           )
+       )  
+
+
+print("""Auto Welcome Bot Started
+Maintained By @Mo_Tech_YT""")
+
+Pr0fess0r_99.run()
